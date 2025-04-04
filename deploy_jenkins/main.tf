f0c1f8b340dfd261f59c94530b050943173f8cc3
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-04-05 05:28:16 +0700

    main.tf: add dynamodb

--- deploy_jenkins/main.tf
+++ deploy_jenkins/main.tf
@@ -113,9 +113,28 @@ resource "aws_instance" "ec2" {
 
 # create s3 bucket for jenkins
 resource "aws_s3_bucket" "terraform-jenkins" {
-    bucket = "terraform-jenkins-109701"
+    bucket = "terraform-jenkins-101001"
+    force_destroy = "true" # Allow force destroy for testing purposes 
     tags = {
         Name = "terraform-jenkins"
         description = "terraform S3 bucket"
     }
+}
+
+# create dynamodb table for state locking
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform_locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "terraform_locks"
+    Environment = "Terraform"
+  }
+  
 }
