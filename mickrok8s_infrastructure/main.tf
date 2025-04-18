1509da27be96b1e1ca075248742f14e199e1282f
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-04-18 16:37:43 +0700

    main.tf: fix error terraform plan 2

--- mickrok8s_infrastructure/main.tf
+++ mickrok8s_infrastructure/main.tf
@@ -200,7 +200,8 @@ resource "aws_launch_configuration" "as_conf" {
 resource "aws_autoscaling_group" "asg-test" {
   name                 = "terraform-asg-example"
   launch_configuration = aws_launch_configuration.as_conf.name
-  vpc_zone_identifier  = data.aws_subnets.all.ids
+  vpc_zone_identifier  = [aws_subnet.microk8s-subnet-1.id, aws_subnet.microk8s-subnet-2.id]
+  desired_capacity     = 1
   min_size             = 2
   max_size             = 5
 
