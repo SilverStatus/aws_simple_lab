b520cf6a92210a39e4889848c8de7951097f4066
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-04-18 17:51:53 +0700

    main.tf: comment terraform state

--- mickrok8s_infrastructure/main.tf
+++ mickrok8s_infrastructure/main.tf
@@ -271,11 +271,11 @@ resource "aws_dynamodb_table" "terraform_locks" {
 }
 
 # enable terraform remote backend and state locking
-terraform {
-  backend "s3" {
-    bucket         = "terraform-microk8s-101001"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform_locks"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "terraform-microk8s-101001"
+#     key            = "terraform.tfstate"
+#     region         = "us-east-1"
+#     dynamodb_table = "terraform_locks"
+#   }
+# }
