4edbd889051ca639e1d93f69cda3cfb5cb224885
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-04-28 22:01:01 +0700

    add ecr creation

--- s3_dynamoDB/main.tf
+++ s3_dynamoDB/main.tf
@@ -42,3 +42,16 @@ resource "aws_dynamodb_table" "terraform_locks" {
   }
   
 }
+
+# create ECR repository
+resource "aws_ecr_repository" "my_ecr_repo" {
+  name = "my-ecr-repo"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = {
+    Name        = "my-ecr-repo"
+    Environment = "Terraform"
+  }
+  
+}
