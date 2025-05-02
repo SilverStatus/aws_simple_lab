289022c16573ce287a5395203ccbdbc06e075d52
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-05-02 13:49:22 +0700

    main.tf: add aws_subnet

--- Microk8s-simple-pipeline/main.tf
+++ Microk8s-simple-pipeline/main.tf
@@ -12,3 +12,16 @@ resource "aws_vpc" "microk8s-vpc" {
   }
 }
 
+resource "aws_subnet" "public_subnet" {
+  count = length(data.aws_availability_zones.available.names)
+  vpc_id = aws_vpc.microk8s-vpc.id
+  cidr_block = cidrsubnet(aws_vpc.microk8s-vpc.cidr_block, 8, count.index)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.project_name}-public-subnet-${count.index}"
+    Environment = "Terraform"
+    Project = "${var.project_name}"
+  }
+  
+}
