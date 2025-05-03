89f745fb07257bb39edabb89a51604f5e5a6cc46
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-05-03 09:53:44 +0700

    main.tf: change security group attachment to alb with id

--- Microk8s-simple-pipeline/main.tf
+++ Microk8s-simple-pipeline/main.tf
@@ -153,7 +153,7 @@ resource "aws_lb" "microk8s_lb" {
   name = "microk8s-lb"
   internal = false 
   load_balancer_type = "application"
-  security_groups = [aws_security_group.alb_sg]
+  security_groups = [aws_security_group.alb_sg.id]
   subnets = aws_subnet.public_subnet[*].id
   enable_deletion_protection = false
 
