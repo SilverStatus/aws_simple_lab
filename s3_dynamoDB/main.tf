00b68225d2cb793c7e142afd94f8e50b32ad553d
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-06-15 20:44:21 +0700

    edit policy for ecr

--- s3_dynamoDB/main.tf
+++ s3_dynamoDB/main.tf
@@ -74,7 +74,11 @@ resource "aws_ecr_repository_policy" "my_ecr_repo_policy" {
       "Action": [
         "ecr:GetDownloadUrlForLayer",
         "ecr:BatchGetImage",
-        "ecr:BatchCheckLayerAvailability"
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:CompleteLayerUpload",
+        "ecr:InitiateLayerUpload",
+        "ecr:PutImage",
+        "ecr:UploadLayerPart"
       ]
     }
   ]
@@ -82,6 +86,7 @@ resource "aws_ecr_repository_policy" "my_ecr_repo_policy" {
 EOF
 }
 
+#data source that queries information about the AWS account and credentials currently in use
 data "aws_caller_identity" "current" {}
 
 # Optional: Lifecycle policy to clean up old images
