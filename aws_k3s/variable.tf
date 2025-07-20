bd56da1cfce6f3c0e77001e34ff60c2fac2e1ff5
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-07-20 20:25:44 +0700

    initial commit

---
+++ aws_k3s/variable.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "AWS region to deploy the K3s cluster"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "k3s-cluster"
+} 
