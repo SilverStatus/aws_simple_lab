4e26ab497f699c7f840466a58bdff642ebff5b93
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-07-27 19:12:53 +0700

    change sg alb to allow port 30000 ingress

--- aws_k3s/main.tf
+++ aws_k3s/main.tf
@@ -158,6 +158,13 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 30000
+    to_port     = 30000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
