1bfed0a95c1ce041d35030da6fb2231a3e9e4daa
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-09-05 14:52:47 +0700

    fix: edit aws_subnet resource block

--- tf_modules_aws/modules/vpc/vpc.tf
+++ tf_modules_aws/modules/vpc/vpc.tf
@@ -19,7 +19,7 @@ resource "aws_vpc" "main_vpc" {
 }
 
 resource "aws_subnet" "public_subnet" {
-  count = length(data.aws_availability_zones.available.names)
+  count = length(data.aws_availability_zones.azs.names)
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = cidrsubnet(var.vpc_cidr_block, 8, count.index)
   availability_zone       = data.aws_availability_zones.azs.names[0]
