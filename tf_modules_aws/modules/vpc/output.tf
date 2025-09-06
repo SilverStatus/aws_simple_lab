5254848dae04a38130ba1ce240d0ef0e1f714f26
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-09-06 11:37:09 +0700

    fix: error when terraform plan ec2.tf

--- tf_modules_aws/modules/vpc/output.tf
+++ tf_modules_aws/modules/vpc/output.tf
@@ -6,12 +6,12 @@ output "public_subnet_ids" {
     value = aws_subnet.public_subnet[*].id
 }
 
-# output "public_subnet_ids" {
-#   value = aws_subnet.public.*.id
-# }
-
 output "route_table_id" {
     value = aws_route_table.public_route_table.id
+}
+
+output "vpc_cidr_block" {
+    value = aws_vpc.main_vpc
 }
 
 
