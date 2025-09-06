a1894b69c7609f13565627d09979b38c2de71d45
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-09-06 12:12:09 +0700

    fix: output.tf add foreach

--- tf_modules_aws/infrastructure/k3s_cluster/output.tf
+++ tf_modules_aws/infrastructure/k3s_cluster/output.tf
@@ -1,3 +1,8 @@
+#create output from vpc module declaration in main.tf
+output "vpc_cidr_block" {
+  value = aws_vpc.main.cidr_block
+}
+
 output "spot_instance_ips" {
   value = module.ec2.k3_instance_spot_public_ips
 }
--- tf_modules_aws/modules/ec2/output.tf
+++ tf_modules_aws/modules/ec2/output.tf
@@ -6,10 +6,10 @@ output "k3_instance_spot_public_ips" {
   value = var.create_spot_instances ? [for instance in aws_instance.k3_instance_spot : instance.public_ip] : []
 }
 
-# output "k3s_instance_on_demand_ids" {
-#   value = var.create_on_demand_instances ? aws_instance.k3s_instance_on_demand[*].id : []
-# }
+output "k3s_instance_on_demand_ids" {
+  value = var.create_on_demand_instances ? [for instance in aws_instance.k3s_instance_on_demand : instance.id] : []
+}
 
-# output "k3s_instance_on_demand_ips" {
-#   value = var.create_on_demand_instances ? aws_instance.k3s_instance_on_demand[*].public_ip : []
-# }
+output "k3s_instance_on_demand_ips" {
+  value = var.create_on_demand_instances ? [for instance in aws_instance.k3s_instance_on_demand : instance.public_ip] : []
+}
