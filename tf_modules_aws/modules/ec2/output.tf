483126458856989c42b6cf24d9688cb054040003
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-09-06 12:07:34 +0700

    fix: output.tf add foreach

--- tf_modules_aws/modules/ec2/output.tf
+++ tf_modules_aws/modules/ec2/output.tf
@@ -1,15 +1,15 @@
 output "k3_instance_spot_ids" {
-  value = var.create_spot_instances ? aws_instance.k3_instance_spot[*].id : []
+  value = var.create_spot_instances ? [for instance in aws_instance.k3_instance_spot : instance.id] : []
 }
 
 output "k3_instance_spot_public_ips" {
-  value = var.create_spot_instances ? aws_instance.k3_instance_spot[*].public_ip : []
+  value = var.create_spot_instances ? [for instance in aws_instance.k3_instance_spot : instance.public_ip] : []
 }
 
-output "k3s_instance_on_demand_ids" {
-  value = var.create_on_demand_instances ? aws_instance.k3s_instance_on_demand[*].id : []
-}
+# output "k3s_instance_on_demand_ids" {
+#   value = var.create_on_demand_instances ? aws_instance.k3s_instance_on_demand[*].id : []
+# }
 
-output "k3s_instance_on_demand_ips" {
-  value = var.create_on_demand_instances ? aws_instance.k3s_instance_on_demand[*].public_ip : []
-}
+# output "k3s_instance_on_demand_ips" {
+#   value = var.create_on_demand_instances ? aws_instance.k3s_instance_on_demand[*].public_ip : []
+# }
