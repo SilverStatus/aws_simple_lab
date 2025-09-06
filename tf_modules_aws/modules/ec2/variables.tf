5cf9e859146fad829563260765e644f0e18bf082
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-09-06 11:48:44 +0700

    fix: remove type string in vpc_cidr_block

--- tf_modules_aws/modules/ec2/variables.tf
+++ tf_modules_aws/modules/ec2/variables.tf
@@ -9,8 +9,7 @@ variable "vpc_id" {
   type        = string
 }
 variable "vpc_cidr_block" {
-  description = "The CIDR block for the VPC"
-  type        = string  
+  description = "The CIDR block for the VPC" 
 }
 
 #identification and tagging variables
