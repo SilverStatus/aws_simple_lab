2f4d909334f79a376bb06fe67728a24c2fb4eed6
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-09-20 19:40:44 +0700

    fix: error when plan

--- tf_modules_aws/modules/eks/main.tf
+++ tf_modules_aws/modules/eks/main.tf
@@ -10,26 +10,26 @@ resource "aws_eks_cluster" "eks_cluster" {
     }
 
     depends_on = [
-    aws_iam_role_policy_attachment.cluster
+    aws_iam_role_policy_attachment.eks_cluster
   ]
 }
 
 resource "aws_eks_addon" "vpc_cni" {
-  cluster_name      = aws_eks_cluster.eks.name
+  cluster_name      = aws_eks_cluster.eks_cluster.name
   addon_name        = "vpc-cni"
   addon_version     = "v1.14.1-eksbuild.1"
   resolve_conflicts_on_create = "OVERWRITE"
 }
 
 resource "aws_eks_addon" "kube_proxy" {
-  cluster_name      = aws_eks_cluster.eks.name
+  cluster_name      = aws_eks_cluster.eks_cluster.name
   addon_name        = "kube-proxy"
   addon_version     = "v1.27.3-eksbuild.1"
   resolve_conflicts_on_create = "OVERWRITE"
 }
 
 resource "aws_eks_addon" "coredns" {
-  cluster_name      = aws_eks_cluster.eks.name
+  cluster_name      = aws_eks_cluster.eks_cluster.name
   addon_name        = "coredns"
   addon_version     = "v1.10.3-eksbuild.1"
   resolve_conflicts_on_create = "OVERWRITE"
--- tf_modules_aws/modules/eks/output.tf
+++ tf_modules_aws/modules/eks/output.tf
@@ -3,17 +3,15 @@ output "cluster_name" {
 }
 
 output "cluster_endpoint" {
-  value = aws_eks_cluster.eks.endpoint
+  value = aws_eks_cluster.eks_cluster.endpoint
 }
 
 output "cluster_version" {
-  value = aws_eks_cluster.eks.version
+  value = aws_eks_cluster.eks_cluster.version
 }
 
 output "cluster_ca_certificate" {
-  value = aws_eks_cluster.eks.certificate_authority[0].data
+  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
 }
 
-output "node_group_role_arn" {
-  value = aws_iam_role.node_group.arn
-}
+
