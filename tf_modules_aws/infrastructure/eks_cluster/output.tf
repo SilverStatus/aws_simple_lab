e1db1a9aca07a11a534e2f7f04f7dc28a2f3df38
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-09-21 06:21:41 +0700

    fix: remove node group arn output.tf

--- tf_modules_aws/infrastructure/eks_cluster/output.tf
+++ tf_modules_aws/infrastructure/eks_cluster/output.tf
@@ -10,7 +10,5 @@ output "eks_cluster_version" {
 output "eks_cluster_ca_certificate" {
   value = module.eks_cluster.cluster_ca_certificate
 }
-output "eks_node_group_role_arn" {
-  value = module.eks_cluster.node_group_role_arn
-}
+
 
