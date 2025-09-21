da048f7f8820a864a650df7fbd04cf9e5e0143bd
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-09-21 17:47:52 +0700

    fix: permission to create eks, change arn

--- tf_modules_aws/modules/iam/main.tf
+++ tf_modules_aws/modules/iam/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "eks_cluster" {
-  name = "${var.aws_eks_cluster_name}-role"
+  name = "eks-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
