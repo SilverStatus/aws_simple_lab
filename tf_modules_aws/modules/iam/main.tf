518e244e7a8dbfd2ae58658f28100bafea43a13d
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-09-27 18:44:50 +0700

    chore: add capability to add principle eks

--- tf_modules_aws/modules/iam/main.tf
+++ tf_modules_aws/modules/iam/main.tf
@@ -51,3 +51,20 @@ resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOn
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.eks_node_group.name
 }
+
+# Create access entries and policy associations for admin user
+resource "aws_eks_access_entry" "admin" {
+  cluster_name      = aws_eks_cluster.example.name
+  principal_arn     = "arn:aws:account::084828586638:account"
+  kubernetes_groups = ["system:masters"]
+  type              = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "admin" {
+  cluster_name  = var.aws_eks_cluster_name
+  principal_arn = "arn:aws:account::084828586638:account"
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  access_scope {
+    type = "ALL"
+  }
+}
