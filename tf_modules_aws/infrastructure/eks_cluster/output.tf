01b03ace9f03ab10e20d479b2df2249e007536fe
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-09-27 18:48:24 +0700

    chore: add capability to add principle eks

--- tf_modules_aws/infrastructure/eks_cluster/output.tf
+++ tf_modules_aws/infrastructure/eks_cluster/output.tf
@@ -16,10 +16,6 @@ output "cluster_endpoint" {
   value       = module.eks_cluster.cluster_endpoint
 }
 
-output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane"
-  value       = module.eks_cluster.cluster_security_group_id
-}
 
 output "region" {
   description = "AWS region"
@@ -28,7 +24,7 @@ output "region" {
 
 output "cluster_name" {
   description = "Kubernetes Cluster Name"
-  value       = module.eks_cluster.cluster_name
+  value       = module.eks_cluster.name
 }
 
 
